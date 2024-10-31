0a60c8d385bf3fb7c772d991f2d931cd3c81d197
Adithya Srinivasan <adithya.srini8912@gmail.com>
2024-10-31 01:13:49 -0400

    Fixed YouTube search, play_song, next_song, prev_song and added genre search

--- bot.py
+++ bot.py
@@ -15,11 +15,11 @@ from src.utils import searchSong, has_role_dj, update_vc_status, check_vc_status
 from src.songs_queue import Songs_Queue
 from src.songs_cog import Songs
 
-load_dotenv('.env')
-TOKEN = os.getenv('DISCORD_TOKEN')
+load_dotenv(".env")
+TOKEN = os.getenv("DISCORD_TOKEN")
 # This can be obtained using ctx.message.author.voice.channel
 
-VOICE_CHANNEL_ID = 1300855852374163497 # Group 86 test VC
+VOICE_CHANNEL_ID = 1300855852374163497  # Group 86 test VC
 # 1299270426794524708 # Enigma test general
 # 1300499798545141873 # Hotel Quink Server
 # 1300287459451736064 - SQUAD Server
@@ -30,12 +30,14 @@ intents.members = True
 intents.message_content = True
 intents.guilds = True
 # client = commands.Bot(command_prefix='/', intents=intents)
-client = commands.Bot(command_prefix=']', intents=intents)
+client = commands.Bot(command_prefix="]", intents=intents)
 authorized_channels = []
 
 """
 Function that gets executed once the bot is initialized
 """
+
+
 @client.event
 async def on_ready():
     voice_channel = client.get_channel(VOICE_CHANNEL_ID)
@@ -48,6 +50,8 @@ async def on_ready():
 """
 Function to reconnect bot if it disconnects
 """
+
+
 @client.event
 async def on_voice_state_update(member, before, after):
     # if member == client.user and before.channel is not None and after.channel is None:
@@ -58,9 +62,9 @@ async def on_voice_state_update(member, before, after):
     #     # Ensure the bot only attempts to connect if it is not already connected
     #     if voice_channel and not voice_channel.is_connected():
     #         await voice_channel.connect(reconnect=True, timeout=10.0)
-    
+
     await update_vc_status(client, member, before, after)
-    
+
 
 client.add_check(check_vc_status())
 
@@ -72,8 +76,10 @@ channels_help = """
 Add allowed channels for the bot 
 - example : ]channels <channel1>, <channel2>, ...
 """
+
+
 # @client.command(name='channels', help='Add allowed channels for the bot')
-@client.command(name='channels', help=channels_help)
+@client.command(name="channels", help=channels_help)
 @has_role_dj()
 async def authorize_channel(ctx):
     global authorized_channels
@@ -85,47 +91,74 @@ async def authorize_channel(ctx):
         for tc in channels_extract:
             if tc not in authorized_channels:
                 authorized_channels.append(tc)
-                new_channels.append(tc)     
+                new_channels.append(tc)
         if new_channels:
-            new_channels_str = ', '.join(new_channels)
+            new_channels_str = ", ".join(new_channels)
             await ctx.send(f"Added authorized channels: {new_channels_str}")
         else:
-            await ctx.send("No new channels added. All channels have been authorized already.")
+            await ctx.send(
+                "No new channels added. All channels have been authorized already."
+            )
     else:
-        await ctx.send("No channels found to authorize. Use format: `]channels <channel1>, <channel2>, ...`")
+        if authorized_channels:
+            await ctx.send(
+                f"No new channels added. \nChannels : {', '.join(f'[{ch_name}]' for ch_name in authorized_channels)}"
+            )
+        else:
+            await ctx.send(
+                f"No new channels added. \nUse format: ]channels <channel1>, <channel2>, ..."
+            )
 
 
 """
 Function for playing a song
 """
-@client.command(name='play', help='To play a song')
+
+
+@client.command(name="play", help="To play a song")
 @has_role_dj()
 async def play(ctx, *, song: str):
     await ctx.send(f"Playing {song} requested by {ctx.author.display_name}")
 
+
 """
 Function that is executed once any message is received by the bot
 """
+
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     options = set()
+
+#     # available_channels = ['general', 't-time', 'testing-space']
+#     if message.channel.name in authorized_channels:
+#         # if message.channel.name == 'testing-space':
+#         user_message = str(message.content)
+#         await client.process_commands(message)
+
+#     if message.channel.name == "testing-space":
+#         user_message = str(message.content)
+#         await client.process_commands(message)
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    options = set()
+    if (
+        message.channel.name in authorized_channels
+        or message.channel.name == "testing-space"
+    ):
+        await client.process_commands(message)
 
-    # available_channels = ['general', 't-time', 'testing-space']
-    if message.channel.name in authorized_channels:
-    # if message.channel.name == 'testing-space':
-        user_message = str(message.content)
-        await client.process_commands(message)
-        
-    if message.channel.name == 'testing-space':
-        user_message = str(message.content)
-        await client.process_commands(message)
-           
 
 """
 Error checking function that returns any error received by the bot
 """
+
+
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, CheckFailure):
@@ -134,11 +167,12 @@ async def on_command_error(ctx, error):
         print(f"An error has occurred: {error}")
 
 
-@client.command(name='reconnect', help='To connect the bot to voice channel')
+@client.command(name="reconnect", help="To connect the bot to voice channel")
 @has_role_dj()
 async def reconnect(ctx):
     await ctx.send("Reconnecting enigma to VC ...")
     await on_ready()
+
 
 """
 Start the bot
--- src/songs_cog.py
+++ src/songs_cog.py
@@ -2,38 +2,58 @@
 This file is responsible for all bot commands regarding songs such ]poll for generating recommendations,
 ]next_song for playing next song and so on
 """
+
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import youtube_dl
+import yt_dlp
+import asyncio
 import sys
+import urllib.parse, urllib.request, re
 
 from src.get_all import *
-from src.utils import searchSong, random_25, has_role_dj
+from src.utils import searchSong, random_25, has_role_dj, check_vc_status
 from src.songs_queue import Songs_Queue
-sys.path.append('/Users/rohitsriram/Documents/Higher Education/North Carolina State University/Curriculum/Year 2/Sem 3/CSC510 Software Engineering/project/Enigma/')
+
+sys.path.append(
+    "/Users/rohitsriram/Documents/Higher Education/North Carolina State University/Curriculum/Year 2/Sem 3/CSC510 Software Engineering/project/Enigma/"
+)
 # from bot import on_ready
 
 
 FFMPEG_OPTIONS = {
-    'before_options':
-    '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
-    'options': [
-        'ffmpeg', '-i', './assets/sample.mp4', '-vn', '-f', 'mp3',
-        './assets/sample.mp3'
-    ]
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    "options": [
+        "ffmpeg",
+        "-i",
+        "./assets/sample.mp4",
+        "-vn",
+        "-f",
+        "mp3",
+        "./assets/sample.mp3",
+    ],
 }
-YDL_OPTIONS = {'format': 'bestaudio/best', 'noplaylist': 'True'}
+ffmpeg_options = {
+    "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",
+    "options": '-vn -filter:a "volume=0.25"',
+}
+
+youtube_base_url = "https://www.youtube.com/"
+youtube_results_url = youtube_base_url + "results?"
+youtube_watch_url = youtube_base_url + "watch?v="
+YDL_OPTIONS = {"format": "bestaudio/best", "noplaylist": "True"}
+ytdl = yt_dlp.YoutubeDL(YDL_OPTIONS)
+songs_queue = []
 
 
 class Songs(commands.Cog):
-    
     """
     Cog for bot that handles all commands related to songs
     """
+
     def __init__(self, bot):
         self.bot = bot
-
 
     # """
     # Function for playing a song
@@ -45,16 +65,17 @@ class Songs(commands.Cog):
     #     user_message = str(ctx.message.content)
     #     song_name = user_message.split(' ', 1)[1]
     #     await self.play_song(song_name, ctx)
-    
-
 
     """
     Function for handling resume capability
     """
-    @commands.command(name='resume', help='Resumes the song')
+
+    @commands.command(name="resume", help="Resumes the song")
     @has_role_dj()
     async def resume(self, ctx):
         voice_client = ctx.message.guild.voice_client
+        if voice_client is None:
+            await ctx.send("The bot is not connected to any voice channel.")
         if voice_client.is_paused():
             await voice_client.resume()
         else:
@@ -62,22 +83,22 @@ class Songs(commands.Cog):
                 "The bot was not playing anything before this. Use play command"
             )
 
-
     """
     Function for playing a custom song
     """
-    @commands.command(name='play_custom', help='To play custom song')
+
+    @commands.command(name="play_custom", help="To play custom song")
     @has_role_dj()
     async def play_custom(self, ctx):
         user_message = str(ctx.message.content)
-        song_name = user_message.split(' ', 1)[1]
-        await self.play_song(song_name, ctx)
-
+        song_name = user_message.split(" ", 1)[1]
+        await self.play_song(ctx, song_name)
 
     """
     Function to stop playing the music
     """
-    @commands.command(name='stop', help='Stops the song')
+
+    @commands.command(name="stop", help="Stops the song")
     @has_role_dj()
     async def stop(self, ctx):
         voice_client = ctx.message.guild.voice_client
@@ -86,33 +107,68 @@ class Songs(commands.Cog):
         else:
             await ctx.send("The bot is not playing anything at the moment.")
 
-
     """
     Helper function for playing song on the voice channel
     """
-    async def play_song(self, song_name, ctx):
-        # First stop whatever the bot is playing
-        await self.stop(ctx)
+
+    # async def play_song(self, song_name, ctx):
+    #     # First stop whatever the bot is playing
+    #     await self.stop(ctx)
+    #     try:
+    #         server = ctx.message.guild
+    #         voice_channel = server.voice_client
+    #         url = searchSong(song_name)
+    #         async with ctx.typing():
+    #             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+    #                 info = ydl.extract_info(url, download=False)
+    #                 I_URL = info["formats"][0]["url"]
+    #                 source = await discord.FFmpegOpusAudio.from_probe(
+    #                     I_URL, **FFMPEG_OPTIONS
+    #                 )
+    #                 voice_channel.play(source)
+    #                 voice_channel.is_playing()
+    #         await ctx.send("**Now playing:** {}".format(song_name))
+    #     except Exception as e:
+    #         await ctx.send("The bot is not connected to a voice channel.")
+    async def play_song(self, ctx, song_name):
         try:
             server = ctx.message.guild
             voice_channel = server.voice_client
-            url = searchSong(song_name)
-            async with ctx.typing():
-                with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                    info = ydl.extract_info(url, download=False)
-                    I_URL = info['formats'][0]['url']
-                    source = await discord.FFmpegOpusAudio.from_probe(
-                        I_URL, **FFMPEG_OPTIONS)
-                    voice_channel.play(source)
-                    voice_channel.is_playing()
-            await ctx.send('**Now playing:** {}'.format(song_name))
         except Exception as e:
-            await ctx.send("The bot is not connected to a voice channel.")
+            print("The user hasn't joined the voice channel.")
+        try:
+            if youtube_base_url not in song_name:
+                query_string = urllib.parse.urlencode({"search_query": song_name})
 
+                content = urllib.request.urlopen(youtube_results_url + query_string)
+
+                search_results = re.findall(
+                    r"/watch\?v=(.{11})", content.read().decode()
+                )
+
+                link = youtube_watch_url + search_results[0]
+
+            loop = asyncio.get_event_loop()
+            data = await loop.run_in_executor(
+                None, lambda: ytdl.extract_info(link, download=False)
+            )
+
+            song = data["url"]
+            song_title = data["title"]
+            print(song_title)
+            player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
+
+            voice_channel.play(player)
+
+        except Exception as e:
+            await ctx.send(
+                "The bot is not connected to any voice channel at the moment."
+            )
 
     """
     Helper function to handle empty song queue
     """
+
     async def handle_empty_queue(self, ctx):
         try:
             songs_queue
@@ -128,71 +184,80 @@ class Songs(commands.Cog):
             return True
         return False
 
-
     """
     Function to play the next song in the queue
     """
-    @commands.command(name='next_song', help='To play next song in queue')
+
+    @commands.command(name="next_song", help="To play next song in queue")
     @has_role_dj()
     async def next_song(self, ctx):
+        global songs_queue
+        voice_client = ctx.message.guild.voice_client
         empty_queue = await self.handle_empty_queue(ctx)
         if not empty_queue:
-            await self.play_song(songs_queue.next_song(), ctx)
-
+            if voice_client.is_playing():
+                await voice_client.pause()
+                await ctx.send("Playing the next song now ...")
+            await self.play_song(ctx, songs_queue.next_song())
 
     """
     Function to play the previous song in the queue
     """
-    @commands.command(name='prev_song', help='To play prev song in queue')
+
+    @commands.command(name="prev_song", help="To play prev song in queue")
     @has_role_dj()
     async def play(self, ctx):
+        global songs_queue
         empty_queue = await self.handle_empty_queue(ctx)
         if not empty_queue:
             await self.play_song(songs_queue.prev_song(), ctx)
 
-
     """
     Function to pause the music that is playing
     """
-    @commands.command(name='pause', help='This command pauses the song')
+
+    @commands.command(name="pause", help="This command pauses the song")
     @has_role_dj()
     async def pause(self, ctx):
         voice_client = ctx.message.guild.voice_client
-        if voice_client.is_playing():
+        if voice_client is None:
+            await ctx.send(
+                "The bot is not connected to any voice channel at the moment."
+            )
+        elif voice_client.is_playing():
             await voice_client.pause()
+            await ctx.send("The song is paused now.")
         else:
             await ctx.send("The bot is not playing anything at the moment.")
-
 
     """
     Function to generate poll for playing the recommendations
     """
-    @commands.command(name='poll', help='Poll for recommendation')
+
+    @commands.command(name="poll", help="Poll for recommendation")
     # @has_role_dj()
     async def poll(self, ctx):
-        reactions = ['👍', '👎']
+        reactions = ["👍", "👎"]
         selected_songs = []
         count = 0
         bot_message = "Select song preferences by reaction '👍' or '👎' to the choices. \nSelect 3 songs"
         await ctx.send(bot_message)
         ten_random_songs = random_25()
-        for ele in zip(ten_random_songs["track_name"],
-                       ten_random_songs["artist"]):
+        for ele in zip(ten_random_songs["track_name"], ten_random_songs["artist"]):
             bot_message = str(ele[0]) + " By " + str(ele[1])
             description = []
-            poll_embed = discord.Embed(title=bot_message,
-                                       color=0x31FF00,
-                                       description=''.join(description))
+            poll_embed = discord.Embed(
+                title=bot_message, color=0x31FF00, description="".join(description)
+            )
             react_message = await ctx.send(embed=poll_embed)
-            for reaction in reactions[:len(reactions)]:
+            for reaction in reactions[: len(reactions)]:
                 await react_message.add_reaction(reaction)
-            res, user = await self.bot.wait_for('reaction_add')
-            if (res.emoji == u'👍'):
+            res, user = await self.bot.wait_for("reaction_add")
+            if res.emoji == "👍":
                 selected_songs.append(str(ele[0]))
                 count += 1
-            if (count == 3):
-                bot_message = "Selected songs are : " + \
-                    ' , '.join(selected_songs)
+            if count == 3:
+                bot_message = "Selected songs are : " + " , ".join(selected_songs)
                 await ctx.send(bot_message)
                 break
         global songs_queue
@@ -200,12 +265,11 @@ class Songs(commands.Cog):
         songs_queue = Songs_Queue(recommended_songs)
         await self.play_song(songs_queue.next_song(), ctx)
 
-
     """
     Function to display all the songs in the queue
     """
-    @commands.command(name='queue',
-                      help='Show active queue of recommendations')
+
+    @commands.command(name="queue", help="Show active queue of recommendations")
     @has_role_dj()
     async def queue(self, ctx):
         empty_queue = await self.handle_empty_queue(ctx)
@@ -218,11 +282,11 @@ class Songs(commands.Cog):
                 else:
                     await ctx.send(queue[i])
 
-
     """
     Function to shuffle songs in the queue
     """
-    @commands.command(name='shuffle', help='To shuffle songs in queue')
+
+    @commands.command(name="shuffle", help="To shuffle songs in queue")
     @has_role_dj()
     async def shuffle(self, ctx):
         empty_queue = await self.handle_empty_queue(ctx)
@@ -230,21 +294,72 @@ class Songs(commands.Cog):
             songs_queue.shuffle_queue()
             await ctx.send("Playlist shuffled")
 
-
     """
     Function to add custom song to the queue
     """
-    @commands.command(name='add_song', help='To add custom song to the queue')
+
+    @commands.command(name="add_song", help="To add custom song to the queue")
     @has_role_dj()
     async def add_song(self, ctx):
         user_message = str(ctx.message.content)
-        song_name = user_message.split(' ', 1)[1]
+        song_name = user_message.split(" ", 1)[1]
         songs_queue.add_to_queue(song_name)
         await ctx.send("Song added to queue")
+
+    """
+    Recommending songs based on genre
+    """
+
+    @commands.command(
+        name="genre", help="To play 5 first songs from the specified genre"
+    )
+    async def genre(self, ctx):
+        user_message = str(ctx.message.content)
+        genre_name = user_message.split(" ", 1)[1]
+        await self.play_song_genre(ctx, genre_name=genre_name)
+
+    async def play_song_genre(self, ctx, *, genre_name):
+        try:
+            server = ctx.message.guild
+            voice_channel = server.voice_client
+        except Exception as e:
+            print("The user hasn't joined the voice channel.")
+        try:
+            url_list = []
+            for i in range(5):
+                if youtube_base_url not in genre_name:
+                    query_string = urllib.parse.urlencode({"search_query": genre_name})
+
+                    content = urllib.request.urlopen(youtube_results_url + query_string)
+
+                    search_results = re.findall(
+                        r"/watch\?v=(.{11})", content.read().decode()
+                    )
+
+                    link = youtube_watch_url + search_results[i]
+
+                loop = asyncio.get_event_loop()
+                data = await loop.run_in_executor(
+                    None, lambda: ytdl.extract_info(link, download=False)
+                )
+
+                song = data["url"]
+                song_title = data["title"]
+                await ctx.send("Playing song : " + song_title)
+                print(song_title)
+                player = discord.FFmpegOpusAudio(song, **ffmpeg_options)
+                voice_channel.play(player)
+
+        except Exception as e:
+            await ctx.send(
+                "The bot is not connected to any voice channel at the moment."
+            )
 
 
 """
     Function to add the cog to the bot
 """
+
+
 async def setup(client):
     await client.add_cog(Songs(client))
